f84fbe682457787d37bc653f936daa933ba079b3
ahmet <ahmetoguzhana@gmail.com>
2023-04-10 14:53:36 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("developing hello feature");
+        System.out.println("github repo is already published to github");
+        System.out.println("doing more work");
     }
 }
